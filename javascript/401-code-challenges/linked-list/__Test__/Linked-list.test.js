39e1cf3c7af40d75812dcae064090e909cb90124
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-10-24 16:40:57 -0700

    added readme

--- javascript/401-code-challenges/linked-list/__Test__/Linked-list.test.js
+++ javascript/401-code-challenges/linked-list/__Test__/Linked-list.test.js
@@ -30,9 +30,7 @@ describe('LinkedList test', () => {
     list.head = new Node(1)
     list.insert(10)
     list.insert(11)
-    expect(list.head.value).toBe(11);
-    expect(list.head.next.value).toBe(10);
-    expect(list.head.next.next.value).toBe(1);
+    expect(list).toEqual({"head": {"next": {"next": {"next": null, "value": 1}, "value": 10}, "value": 11}});
   });
 
   it('Will return true when finding a value within the linked list that exists', () => {
@@ -55,14 +53,10 @@ describe('LinkedList test', () => {
 
   it('Can properly return a collection of all the values that exist in the linked list', () => {
     let list = new LinkedList();
-    list.head = new Node(1)
-    list.head.next = new Node(2)
-    list.head.next.next = new Node(3)
-    list.head.next.next.next = new Node(4)
-    expect(list.head.value).toBe(1);
-    expect(list.head.next.value).toBe(2);
-    expect(list.head.next.next.value).toBe(3);
-    expect(list.head.next.next.next.value).toBe(4);
+    list.insert(1)
+    list.insert(2)
+    list.insert(3)
+    expect(list).toEqual({"head": {"next": {"next": {"next": null, "value": 1}, "value": 2}, "value": 3}});
   });
 
 });
