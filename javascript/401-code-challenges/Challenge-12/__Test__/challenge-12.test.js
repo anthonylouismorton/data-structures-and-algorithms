66f06e2c6d8432683c6132d8b467cc1e5cdc43e6
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-11-03 08:13:58 -0700

    did some stuff

--- javascript/401-code-challenges/Challenge-12/__Test__/challenge-12.test.js
+++ javascript/401-code-challenges/Challenge-12/__Test__/challenge-12.test.js
@@ -1,88 +1,97 @@
 'use strict'
 
 const AnimalShelter = require('../AnimalShelter.js');
-const Stack = require('../Stack.js')
+const Queue = require('../Queue.js')
 
 describe('Testing PsuedoQueue method)', () => {
    it('Can successfully enqueue a cat', () => {
     const newQueue = new AnimalShelter();
     newQueue.enqueue('cat');
-    expect(newQueue.catStack.top.value).toEqual('cat');
+    expect(newQueue.catQueue.front.value).toEqual('cat');
   });
 
-  it('Can enqueue two cats', () => {
+  it('Can successfully enqueue multiple cats', () => {
     const newQueue = new AnimalShelter();
     newQueue.enqueue('cat');
     newQueue.enqueue('cat');
-    expect(newQueue.catStack.top.next.value).toEqual('cat');
+    expect(newQueue.catQueue.front.value).toEqual('cat');
+    expect(newQueue.catQueue.back.value).toEqual('cat');
   });
 
-  it('can dequeue a cat', () => {
-    const newQueue = new AnimalShelter();
-    newQueue.enqueue('cat');
-    newQueue.dequeue('cat');
-    expect(newQueue.catStack.top).toBeNull();
-  });
-  it('can dequeue multiple cats', () => {
+  it('Can successfully dequeue a cat', () => {
     const newQueue = new AnimalShelter();
     newQueue.enqueue('cat');
     newQueue.enqueue('cat');
-    newQueue.enqueue('cat');
     newQueue.dequeue('cat');
-    newQueue.dequeue('cat');
-    expect(newQueue.catStack.top).toBeNull();
-    expect(newQueue.animalQueueStack.top.value).toEqual('cat');
+    expect(newQueue.catQueue.back.next).toBeNull();
+  });
 
+  it('Can successfully dequeue a dog', () => {
+    const newQueue = new AnimalShelter();
+    newQueue.enqueue('dog');
+    newQueue.enqueue('dog');
+    newQueue.dequeue('dog');
+    expect(newQueue.dogQueue.back.next).toBeNull();
   });
 
   it('Can successfully enqueue a dog', () => {
     const newQueue = new AnimalShelter();
     newQueue.enqueue('dog');
-    expect(newQueue.dogStack.top.value).toEqual('dog');
+    expect(newQueue.dogQueue.front.value).toEqual('dog');
   });
 
-  it('Can enqueue two dogs', () => {
+  it('Can successfully enqueue multiple dogs', () => {
     const newQueue = new AnimalShelter();
     newQueue.enqueue('dog');
     newQueue.enqueue('dog');
-    expect(newQueue.dogStack.top.next.value).toEqual('dog');
+    expect(newQueue.dogQueue.front.value).toEqual('dog');
+    expect(newQueue.dogQueue.back.value).toEqual('dog');
   });
 
-  it('can dequeue a dog', () => {
+  it('Can successfully enqueue a mix of dogs and cats', () => {
     const newQueue = new AnimalShelter();
     newQueue.enqueue('dog');
-    newQueue.dequeue('dog');
-    expect(newQueue.dogStack.top).toBeNull();
-  });
-
-  it('Cannot enqueue animals other than dogs and cats', () => {
-    const newQueue = new AnimalShelter();
-    // newQueue.enqueue('dog');
-
-    expect(newQueue.enqueue('rabbit')).toEqual("we do not accept that animal at our shelter");
-  });
-
-  it('Can successfully enqueue a dog and cat', () => {
-    const newQueue = new AnimalShelter();
     newQueue.enqueue('cat');
     newQueue.enqueue('dog');
-    expect(newQueue.catStack.top.value).toEqual('cat');
-    expect(newQueue.dogStack.top.value).toEqual('dog');
+    newQueue.enqueue('cat');
+    expect(newQueue.catQueue.front.value).toEqual('cat');
+    expect(newQueue.catQueue.back.value).toEqual('cat');
+    expect(newQueue.dogQueue.front.value).toEqual('dog');
+    expect(newQueue.dogQueue.back.value).toEqual('dog');
   });
 
-  it('Cannot dequeue an empty queue', () => {
+  it('Can successfully dequeue a mixture of dogs and cats', () => {
     const newQueue = new AnimalShelter();
-    newQueue.enqueue('cat')
-    newQueue.dequeue('cat')
-    expect(newQueue.dequeue('cat')).toEqual("this queue is empty");
+    newQueue.enqueue('dog');
+    newQueue.enqueue('dog');
+    newQueue.enqueue('dog');
+    newQueue.enqueue('cat');
+    newQueue.enqueue('cat');
+    newQueue.enqueue('cat');
+    newQueue.dequeue('dog');
+    newQueue.dequeue('cat');
+    newQueue.dequeue('dog');
+    newQueue.dequeue('cat');
+    expect(newQueue.dogQueue.back.next).toBeNull();
+    expect(newQueue.dogQueue.back.value).toBe('dog');
+    expect(newQueue.catQueue.back.next).toBeNull();
+    expect(newQueue.catQueue.back.value).toBe('cat');
   });
 
-  it('Cannot dequeue an incorrect preference', () => {
+  it('enqueue cannot accept a value other than cat or dog', () => {
     const newQueue = new AnimalShelter();
-    newQueue.enqueue('cat')
+    expect(newQueue.enqueue('rabbit')).toBe("we do not accept that animal at our shelter");
+  });
+
+  it('dequeue cannot accept a value other than cat or dog', () => {
+    const newQueue = new AnimalShelter();
     expect(newQueue.dequeue('rabbit')).toBeNull();
   });
 
+  it('dequeue cannot dequeue and empty queue', () => {
+    const newQueue = new AnimalShelter();
+    expect(newQueue.dequeue('dog')).toBe("the animal shelter is already empty");
+  });
 
   });
 
